4d3eec21c79c0a439a1bf1bf3a4a47350d6054cd
Bewin007 <bewinfelix@karunya.edu.in>
2023-07-29 08:00:45 -0400

    a

--- backend/api/views.py
+++ backend/api/views.py
@@ -38,15 +38,10 @@ class nmap_api(APIView):
         elif scan_type == "range of host":
             start_ip = request.data.get('start_ip')
             ip_count = int(request.data.get('ip_count'))
-            # Calculate the end IP address based on the start_ip and ip_count
             start_ip_parts = start_ip.split('.')
             end_ip_parts = start_ip_parts[:-1] + [str(int(start_ip_parts[-1]) + ip_count - 1)]
             end_ip = '.'.join(end_ip_parts)
-
-            # Construct the IP range for the Nmap command
             ip_range = f"{start_ip}-{end_ip}"
-
-            # Run the Nmap command with the IP range
             command = ['sudo', 'nmap'] + ip_range.split('-')
             a = subprocess.run(command, capture_output=True, text=True, check=True, input='root@2004\n', encoding='utf-8')
             return Response(a.stdout)
